d6eafae1b8bf3bf681561ee51a3d8591bc086fb1
Eirik Meland <eirikm@conduct.no>
2012-06-30 22:53:02 +0200

    installer postgres og opprett database

--- vagrant/familie-hub-server/manifests/init.pp
+++ vagrant/familie-hub-server/manifests/init.pp
@@ -2,14 +2,15 @@ group { "puppet":
     ensure => "present",
 } 
 
-#class {'postgresql::server':
-#    version => '9.1',
-#}
-#
-#postgresql::db { 'familiehub':
-#    owner    => 'familiehub',
-#    password => 'hemmelig',
-#    locale   => 'en_US.UTF-8',
-#    encoding => 'UTF8',
-#}
+class {'postgresql::server':
+    version => '9.1',
+    listen => '*'
+}
+
+postgresql::db { 'familiehub':
+    owner    => 'familiehub',
+    password => 'hemmelig',
+    locale   => 'en_US.UTF-8',
+    encoding => 'UTF8',
+}
 
