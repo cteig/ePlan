34b44a7c5162ec93141f68020c609310def5fbc1
Eirik Meland <eirikm@conduct.no>
2012-06-30 22:16:42 +0200

    klargjøring av vagrant

---
+++ vagrant/familie-hub-server/manifests/init.pp
@@ -0,0 +1,15 @@
+group { "puppet":
+    ensure => "present",
+} 
+
+#class {'postgresql::server':
+#    version => '9.1',
+#}
+#
+#postgresql::db { 'familiehub':
+#    owner    => 'familiehub',
+#    password => 'hemmelig',
+#    locale   => 'en_US.UTF-8',
+#    encoding => 'UTF8',
+#}
+
