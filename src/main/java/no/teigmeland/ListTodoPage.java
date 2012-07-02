e82654214ac70909d349514f5e9d0f963555ed0e
Christine Teig <christine.teig@gmail.com>
2012-07-02 23:32:59 +0200

    Hent liste av TODOs fra WebPage

--- src/main/java/no/teigmeland/ListTodoPage.java
+++ src/main/java/no/teigmeland/ListTodoPage.java
@@ -2,13 +2,29 @@ package no.teigmeland;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ListTodoPage extends WebPage {
 
     public ListTodoPage(PageParameters parameters) {
         super(parameters);
 
-        add(new Label("en_todo", "En ny TODO!"));
+        add(createTodoListView(Arrays.asList("todo 1", "todo 2", "todo3")));
+    }
+
+    private ListView createTodoListView(final List<String> todoListe) {
+        return new ListView("todoListView", todoListe) {
+            @Override
+            protected void populateItem(ListItem listItem) {
+                String todoTekst = (String) listItem.getModelObject();
+
+                listItem.add(new Label("todoListItem", todoTekst));
+            }
+        };
     }
 }
