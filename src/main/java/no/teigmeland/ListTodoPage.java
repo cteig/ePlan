cd87556c6c6b93c06ff4b07efa21e8fa41086425
Christine Teig <christine.teig@gmail.com>
2012-07-02 23:15:35 +0200

    Hent TODO fra WebPage istedenfor å hardkode i html :-)

--- src/main/java/no/teigmeland/ListTodoPage.java
+++ src/main/java/no/teigmeland/ListTodoPage.java
@@ -1,7 +1,14 @@
 package no.teigmeland;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class ListTodoPage extends WebPage {
 
+    public ListTodoPage(PageParameters parameters) {
+        super(parameters);
+
+        add(new Label("en_todo", "En ny TODO!"));
+    }
 }
